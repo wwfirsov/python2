9c2c592ab419b04e08ba0a069335f5d6f2cd7fee
wwfirsov <65654471+wwfirsov@users.noreply.github.com>
2021-04-15 12:46:49 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -8,16 +8,22 @@ def txt_file():
         print('Такой файл уже существует!')
     else:
         with open('file.txt', 'w', encoding='utf-8') as g:
-            #list_text = ['a', 'b', 'c', 'd', 'i', 'f']
             list_text = [random.choice(string.ascii_letters) for i in range(7)]
             list_int = [i for i in range(7)]
             dict = list(zip_longest(list_text, list_int, fillvalue='None'))
-            print('Каталог: ', dict)
             for i in dict:
-                print('Значение i: ', i)
                 g.write(str(i))
                 g.write('\n')
+        g.close()
+        path_file = os.path.join('file.txt')
 
+    path_file_fn(path_file)
+    return path_file
+
+def path_file_fn(path_file):
+    f = open(path_file, 'r', encoding='UTF-8')
+    print(f.readlines())
+    f.close()
 
 txt_file()
 
